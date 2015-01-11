ef52bb8db75be5b2cacdfd1871962b24c1f9492e
Ankit Mangal <ankitmangal04@gmail.com>
2015-01-11 10:33:38 -0600

    Delete plot1.R

--- plot1.R
+++
@@ -1,9 +0,0 @@
-
-dataset <- read.table("C:\\Users\\amanga1\\Downloads\\Exploratory data analysis\\working_dir\\household_power_consumption.txt", header = TRUE, sep = ";",stringsAsFactors = FALSE)
-subset_sm  <- dataset[ dataset$Date %in% c("1/2/2007","2/2/2007"),]
-gap <- as.numeric(subset_sm$Global_active_power)
-to_plot <- na.omit(gap)
-
-png("plot1.png", width=480, height=480)
-hist(to_plot, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
